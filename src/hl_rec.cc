08540b30bcc056565787bda868cf446e35828a4d
Eddie Barton <eddiebarton@zoho.com>
2020-05-03 13:59:24 -0500

    Updated whitelist

--- src/hl_rec.cc
+++ src/hl_rec.cc
@@ -14,9 +14,26 @@ int main(int argc, char **argv) {
   double total_time = 0.;
 
   std::vector<std::string> list = {
+      "62CL_20150215_180549_095",
       "6G7M_20150403_165723_505",
+      "5a9e_20150404_122216_530",
+      "c1b1_20150311_204258_018",
+      "0830_20151101_123037_908",
+      "0155_20160903_150944_775",
+      "0030_20151006_135654_478",
+      "47L8_20150517_182450_843",
+      "4WBR_20150516_112748_501",
+      "0006_20160726_142655_865",
       "0132_20160917_184610_200",
-      "c1b1_20150408_154240_395"
+      "0043_20161005_164046_315",
+      "c1b1_20150408_154240_395",
+      "0043_20160922_133339_966",
+      "0032_20160921_125350_046",
+      "33TJ_20150705_191438_366",
+      "JN34_20150319_183334_317",
+      "6G7M_20150421_121002_835",
+      "0919_20150905_182209_361",
+      "0830_20151108_120206_194"
   };
 
   for (auto entry = fs::recursive_directory_iterator(input_directory);
@@ -34,7 +51,7 @@ int main(int argc, char **argv) {
       dng.erase(remove(dng.begin(), dng.end(), '\"'), dng.end());
 
       std::cout << dng << std::endl;
-      if (std::find(list.begin(), list.end(), dng) != list.end()) {
+      if (list.empty() || std::find(list.begin(), list.end(), dng) != list.end()) {
         auto time = process(filepath, parent);
         total_time += time;
         ++total_dng;
